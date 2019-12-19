1555ed3cbad5f154508c0c50d38bbdff50d7c239
Thai Son Nguyen <thaison.nguyen@ncs.com.sg>
2019-12-19 23:05:19 +0800

    Create healthcheck servlet, this will be used by ELB to determine if the zone is healthy or not

---
+++ core/src/main/java/com/discovery/sea/afn/servlets/SimpleHealthCheckServlet.java
@@ -0,0 +1,35 @@
+package com.discovery.sea.afn.servlets;
+
+import static org.apache.sling.api.servlets.ServletResolverConstants.SLING_SERVLET_EXTENSIONS;
+import static org.apache.sling.api.servlets.ServletResolverConstants.SLING_SERVLET_METHODS;
+import static org.apache.sling.api.servlets.ServletResolverConstants.SLING_SERVLET_PATHS;
+import static org.apache.sling.api.servlets.ServletResolverConstants.SLING_SERVLET_SELECTORS;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.osgi.service.component.annotations.Component;
+
+@Component(service = Servlet.class,
+property = 
+{ SLING_SERVLET_PATHS + "=/services/healthcheck",
+  SLING_SERVLET_METHODS + "=GET", 
+  SLING_SERVLET_SELECTORS + "=simple",
+  SLING_SERVLET_EXTENSIONS + "=html" })
+public class SimpleHealthCheckServlet extends SlingSafeMethodsServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(final SlingHttpServletRequest req,
+            final SlingHttpServletResponse resp) throws ServletException, IOException {
+    	resp.setStatus(HttpStatus.SC_OK);
+        resp.getWriter().write("OK");
+    }
+}
